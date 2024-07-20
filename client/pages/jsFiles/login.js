c2bd3039c8365b7bc589379065b38eb66c6666c3
mkk100 <tommychimes2003@gmail.com>
2024-07-20 10:03:05 -0700

    first endpoint

--- client/pages/jsFiles/login.js
+++ client/pages/jsFiles/login.js
@@ -1,6 +1,10 @@
 
-function login(){
+async function login(){
     const email = document.getElementById('email')
     const pw = document.getElementById('pw')
-    console.log(email)
+    const res = await fetch('http://localhost:4000/test',{
+        method: "POST",
+        body: JSON.stringify({email: email, pw: pw})
+    })
+    console.log(res.json())
 }
--- client/pages/jsFiles/register.js
+++ client/pages/jsFiles/register.js
@@ -1,6 +1,12 @@
-
-function register(){
-    const email = document.getElementById('email')
-    const pw = document.getElementById('pw')
-    console.log(email)
+async function register(){
+    const email = document.getElementById('email').value
+    const pw = document.getElementById('pw').value
+    const res = await axios.post('http://localhost:4000/test',{
+        email: email,
+        pw: pw,
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+    console.log(res)
 }
